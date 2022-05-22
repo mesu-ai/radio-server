97f67918b3f14577402bce0ad3cda84801f79a19
mesu-ai <mesu.s.ss@gmail.com>
2022-05-22 15:06:59 +0600

    udpdate funtin

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express=require('express');
 const app=express();
 const cors=require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const ObjectId=require('mongodb').ObjectId;
 require('dotenv').config();
 
 const port=process.env.PORT || 5000;
@@ -25,13 +26,62 @@ async function run() {
     const database =await client.db("radio");
     const stationCollection= database.collection('radio-station');
 
+    // get station
+    app.get('/stations',async(req,res)=>{
+        const result=await stationCollection.find({}).toArray();
+        res.send(result);
+
+    });
+    app.get('/stations/:id',async(req,res)=>{
+        const id=req.params.id;
+        const query={_id:ObjectId(id)}
+        const result=await stationCollection.findOne(query);
+        res.send(result);
+
+    });
+
+    //add station
+    app.post('/stations',async(req,res)=>{
+        const station=req.body;
+        const result=await stationCollection.insertOne(station);
+        res.json(result);
+
+    });
+
+    //delete station
+    app.delete('/stations/:id',async(req,res)=>{
+        const id=req.params.id;
+        
+        const query={_id:ObjectId(id)};
+        const result=await stationCollection.deleteOne(query);
+        res.json(result);
+
+    });
+
+    // update station
+    app.put('/stations/:id',async(req,res)=>{
+        const station=req.body;
+        const id=req.params.id;
+        const filter={_id:ObjectId(id)};
+        const options = { upsert: true };   
+        const updateDoc={$set:{
+            name:station.name,
+            frequency:station.frequency
+
+        }};
+        const result=await stationCollection.updateOne(filter,updateDoc,options);
+        
+        res.json(result);
+               
+    });
+
 
     console.log("Connected successfully to server");
 
   } finally {
 
     
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
